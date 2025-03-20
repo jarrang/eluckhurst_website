9ee72d64b45c1b8fe092031c4ea63bc1e0b2865e
Rob Moran <rob@jarrang.com>
2025-03-20 15:05:21 +0000

    Migrated files from Plesk

---
+++ index.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Fresh Produce Advisory Ltd - Ellis Luckhurst</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Fresh Produce Advisory Ltd offers a selection of services for fresh produce growers &amp; packers">
+    <link rel="icon" href="img/favicon.png" type="image/x-icon">
+	<link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="css/custom.css" />
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
+    <script src="js/vendor/modernizr.js"></script>
+	</head>
+	<body>
+	<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-PFDWMF"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PFDWMF');</script>
+<!-- End Google Tag Manager -->
+
+		<div class="wrapper">
+			<div class="row">
+				<div class="small-6 medium-6 columns small-text-center medium-text-left">
+					<a href="/"><img src="img/Fresh-Produce_Advisory-Ltd-Logo.jpg" width="145" /></a>
+				</div>
+				<div class="small-6 medium-6 columns">
+					<p class="header-details text-right small-text-center medium-text-right">Call on: 07973 626934<br/>
+					<a class="hide-for-small" href="mailto:ellis@eluckhurst.co.uk">ellis@eluckhurst.co.uk</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="photo-wrapper">
+			<div class="wrapper">
+				<div class="row">
+					<div class="small-12 columns">
+						<h1 class="white-bg">A selection of services we can offer</h1>
+						<h1 class="white-bg">to fresh produce growers &amp; packers</h1>
+					</div>
+					<div class="small-12 medium-5 columns">
+						<h2 class="green-bg">For Growers</h2>
+						<ul class="main-bullets">
+							<li>Agronomy</li>
+							<li>Analytical Testing</li>
+							<li>Crop Programming</li>
+							<li>Farm Assurance Compliance</li>
+						</ul>
+					</div>
+					<div class="small-12 medium-7 columns">
+						<h2 class="green-bg">For Packers</h2>
+						<ul class="main-bullets">
+							<li>Quality Assurance Systems</li>
+							<li>BRC Compliance</li>
+							<li>Third Party Auditing</li>
+							<li>New Product Development</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--about-->
+		<div class="wrapper">
+			<div class="row">
+				<div class="small-12 medium-8 columns">
+					<h2 class="grey-bg">About Ellis Luckhurst</h2>
+					<h3>BSc (Hons); MPhil; MBPR; N.Sch</h3>
+					<p>Independent Technical Advisor to Growers &amp; Packers</p>
+
+					<div class="small-12 medium-3 columns" style="padding-left:0px;">
+						<h3>BASIS No:</h3>
+						<p>E4699/ICMV</p>
+						<h3>FACTS No:</h3>
+						<p>FE/2785</p>
+					</div>
+
+					<div class="small-12 medium-8 columns" style="padding-left:0px;">
+						<h3>BRC Trained Auditor No:</h3>
+						<p>TPA/143</p>
+						<h3>VAT Registration No:</h3>
+						<p>873 9451 82</p>
+					</div>
+				</div>
+				<div class="small-12 medium-4 columns hide-for-small">
+					<a href="http://uk.linkedin.com/pub/ellis-luckhurst/31/789/80a" target="_blank"><img src="img/Ellis-Luckhurst.png" width="100%" class="profile-image"></a>
+					<a href="http://uk.linkedin.com/pub/ellis-luckhurst/31/789/80a" target="_blank"><img src="img/linkedin.png" style="position:relative; display:block; margin-top:-50px; margin:auto;"></a>
+				</div>
+			</div>
+		</div>
+		<!--end about-->
+		<!--footer-->
+		<div class="footer-wrapper">
+			<footer>
+				<div class="row">
+					<div class="small-12 medium-4 columns">
+						<p><strong>Address</strong><br/>
+						St Mary's Chapel<br/>
+						Kenwyn Road<br/>
+						Truro<br/>
+						Cornwall<br/>
+						TR1 3SQ</p>
+					</div>
+					<div class="small-12 medium-4 columns hide-for-small">
+						<p><strong>Links</strong><br/>
+						<a href="http://www.hdc.org.uk/" target="_top">HDC</a><br/>
+						<a href="http://www.nickerson-zwaan.com/" target="_top">Nickerson UK</a><br/>
+						<a href="http://www.rivieraproduce.eu/" target="_top">Riviera Produce Ltd</a><br/>
+						<a href="http://loveyourgreens.co.uk" target="_top">Brassica Growers Association</a><br/>
+						<a href="http://www.nuffieldscholar.org/" target="_top">Nuffield Farming Scholarships Trust</a></p>
+					</div>
+					<div class="small-12 medium-4 columns small-text-left medium-text-right hide-for-small">
+						<img src="img/footer-logo.png">
+					</div>
+				</div>
+				
+			</footer>
+		</div>
+		
+		<!--end footer-->
+		<script src="js/vendor/jquery.js"></script>
+	    <script src="js/foundation.min.js"></script>
+	    <script>
+	      $(document).foundation();
+	    </script>
+	</body>
+</html>
